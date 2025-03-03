f27636d54481d229bbbb8da298838ebfadbaaf66
Mia Pfaff <miapfaff@udel.edu>
2025-03-03 17:54:24 -0500

    Finished reveal answer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
-import { ChangeType } from "./components/ChangeType";
+//import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
+// import { StartAttempt } from "./components/StartAttempt";
+// import { TwoDice } from "./components/TwoDice";
+// import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 
 function App(): JSX.Element {
@@ -13,18 +13,20 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <hr></hr>
-            <Counter></Counter>
             <hr />
-            <RevealAnswer></RevealAnswer>
+            <Counter />
             <hr />
-            <StartAttempt></StartAttempt>
+
+            {/* Correct JSX Commenting */}
+            <RevealAnswer />
             <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
+            {/* <StartAttempt /> */}
+            {/* <hr /> */}
+            {/* <TwoDice /> */}
+            {/* <hr /> */}
+            {/* <ChangeType /> */}
+            {/* <hr /> */}
+            {/* <CycleHoliday /> */}
         </div>
     );
 }
--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,16 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        setIsVisible((prev) => !prev);
+    };
+
+    return (
+        <div>
+            <Button onClick={toggleVisibility}>Reveal Answer</Button>
+            {isVisible && <div>42</div>}
+        </div>
+    );
 }
